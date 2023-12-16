74a8d68504a6c351af501ba789376dd5c2f4478c
Mohshin Chowdhury <mohshinchowdhury@mohshins-mbp-2.mynetworksettings.com>
2023-12-15 20:57:59 -0500

    revert changes

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,10 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "function": {
+    "mochowecom1687423d": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
