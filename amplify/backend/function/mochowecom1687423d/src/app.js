b55837d8456b447d7ceba78a378070d24d03c301
Mohshin Chowdhury <mohshinchowdhury@mohshins-mbp-2.mynetworksettings.com>
2024-01-19 15:24:44 -0500

    test routes locally using nodemon

--- amplify/backend/function/mochowecom1687423d/src/app.js
+++ amplify/backend/function/mochowecom1687423d/src/app.js
@@ -7,6 +7,10 @@ const app = express();
 // Use product routes
 app.use('/main', productRoutes);
 
+app.listen(3000,(req,res)=>{
+    console.log("Server is running on port 3000");
+})
+
 // Other middleware and routes...
 
 module.exports = app;
